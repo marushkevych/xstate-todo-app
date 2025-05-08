47e8ae1f391c525d8d263b50a9f3b78c3f4dde90
Marushkevych <marushkevych@gmail.com>
2025-05-08 18:29:32 -0400

    create new todo

--- src/app/machines/todoAppMachine.js
+++ src/app/machines/todoAppMachine.js
@@ -1,12 +1,13 @@
 import { createMachine, assign } from 'xstate'
 
 export const todosMachine = createMachine({
-/** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAICyBDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALradiUAAdZxVesMgAHogBMW2xS0B2AGwBGV88-OALB4BWVwAaEABPRABaXwoATgDfAGYA+Pd3AA5kgOdEgF9c0KEcAhJOCXpSRiE2MAAnWtRaiiMAGxUAM0aAWwoikVLxWgqqmTkFJXNNXX1LE1gzNVJLGwRIxMcArVdXRIytX3SE9K3QiIQ09Ip-dP8t9y1E2OP3fIKQUnQ4Sz6SsVnTSbLKKxLRORJaLYg9K2WJPdy2U7A5xXdz+ZzOGFaWLOFKufKFGTFURlIZSar-eaApDWKK+dwUeHpVwBTK+CEBWyJRGrWJxVFeHYY9yxWyeWz4kA-YmCUaYcqQCkLCzUlbgxIUVy2Vz+Xyxe6CkLhRCeemJdGxVyw+IuG7it5SgbUUmVaQsTAAUXqjQV1LmSqWKtpjgeEMtWmhsPS8IC3Ps9ItiWZ2OFqN8CVeuSAA */
+/** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAICyBDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALradiUAAdZxVesMgAHogBMW2xS0B2AGwBGV88-OALB4BWVwAaEABPRABaXwoATgDfAGYA+Pd3AA5kgOdEgF9c0KEcAhJOCXpSRiE2MAAnWtRaiiMAGxUAM0aAWwoikVLxWgqqmTkFJXNNXX1LE1gzNVJLGwRIxMcArVdXRIytX3SE9K3QiIQ09Ip-dP8t9y1E2OP3fMKZYtFOasxyyFZ8WpgFRgAByYAA7jMkCA5gsLNCVsl3BR3L4AhlEv5EodXLYQuFEO5HnE0Q5Eq4tLF4g9nK8QH0SmIKABhQEqKTkcGYZQyCgAZUIqHBUk6tS6mDIRgArspWKKugBJUjS5TMwi4SqQKHGUyTZaIXyUpzOdJE9wpWIPbGnRDJLQUHaxXy+PEBXaW5y0umkdBwSwMz5gWa6xb61aWpyJLRbS3pWxU022G3h5xXVFeZzxynOFKuOkBgbUIZSarB+Z6hFRXzI9y2dKuAKZQ1aAK2RLJyKxOLptyJTPuWK2Ty2fPvfpM76-CBluFLSsIdaXBzOKkJPGotsBZM3CiHJ1E1ueqNE0cYD6F8ol0aYACi9UakBnFdAK2ijge0dcsfjT1rW4JCD2MisQ7A2sTeLEqJor4p7CIynCskCqiVJgnLcjIT6hvO6zIn2mz7AEKQODczjJkBDqPM6Wj1kOSTpLEsHnkyiHsihaE8hg-KCsKKHyhKyoyph8IvraX4UHh0ZokRda+KRAF1pcRJOtWOZuF4eT5LkQA */
   id: "Todo Machine",
   initial: "Loading Todos",
   context: {
     todos: [],
-    errorMessage: null
+    errorMessage: null,
+    createNewTodoFormInput: null
   },
   states: {
     "Loading Todos": {
@@ -27,8 +28,25 @@ export const todosMachine = createMachine({
       }
     },
 
-    "Todos Loaded": {},
-    "Loading Todos Errored": {}
+    "Todos Loaded": {
+      on: {
+        createNew: "Creating new todo"
+      }
+    },
+    "Loading Todos Errored": {},
+    "Creating new todo": {
+      states: {
+        "Showing form input": {
+          on: {
+            formInputChanged: {
+              actions: 'assingFormInputToContext'
+            }
+          }
+        }
+      },
+
+      initial: "Showing form input"
+    }
   },
 },{
   actions: {
@@ -40,6 +58,11 @@ export const todosMachine = createMachine({
     assingErrorToContext: assign((conext, event) => {
       return {
         errorMessage: event.data.message
+      }
+    }),
+    assingFormInputToContext: assign((conext, event) => {
+      return {
+        createNewTodoFormInput: event.value
       }
     })
   }
--- src/app/page.js
+++ src/app/page.js
@@ -15,6 +15,18 @@ export default function Home() {
     <div>
       <pre>{JSON.stringify(state.value)}</pre>
       <pre>{JSON.stringify(state.context)}</pre>
+
+      <div>
+        {state.matches('Loading Todos') && <span>Loading...</span>}
+        {state.matches('Todos Loaded') 
+          && <button onClick={()=> send({type: 'createNew'})}>Create New</button>}
+        {state.matches('Creating new todo.Showing form input')
+          && <input onChange={(e)=> send({
+            type: 'formInputChanged',
+            value: e.target.value
+          })}></input>
+        }
+      </div>
     </div>
   );
 }
